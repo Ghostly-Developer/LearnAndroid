bfee6fa9c308b51bcf39310d44b1ba0ac2c338b3
Aman Gupta <amangupta2242@gmail.com>
2025-08-30 00:52:43 +0530

    Greeting test project Done

--- app/src/main/java/com/ghostlydeveloper/project1/MainActivity.kt
+++ app/src/main/java/com/ghostlydeveloper/project1/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ghostlydeveloper.project1.ui.theme.Project1Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +35,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Project1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    GreetingImage(
+                        name = stringResource(R.string.name_test),
+                        by = stringResource(R.string.owner_text)
                     )
                 }
             }
@@ -31,17 +50,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingImage(name: String, by: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.bg)
+    Box{
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+        )
+        Greeting(
+            name = name,
+            by = by,
+            modifier = modifier
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greeting(name: String, by: String, modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ){
+        Text(
+            text = "Good Morning $name!",
+            fontSize = 50.sp,
+            lineHeight = 50.sp,
+            textAlign = TextAlign.Center,
+            color = Color.Black,
+            modifier = modifier.padding(top = 20.dp)
+        )
+        Text(
+            text = "By $by",
+            fontSize = 30.sp,
+            color = Color.Blue,
+            modifier = modifier
+                .padding(end = 10.dp)
+                .align(Alignment.End)
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     Project1Theme {
-        Greeting("Android")
+        GreetingImage("Aman","Me")
     }
 }
--- app/src/main/java/com/ghostlydeveloper/project1/ui/theme/Color.kt
+++ app/src/main/java/com/ghostlydeveloper/project1/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.ghostlydeveloper.project1.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val White = Color(0xFFFFFE)
+val Black = Color(0x16161F)
+val LightBlue = Color(0xE3F6F5)
+val Blue = Color(0x3da9fc)
+val Grey = Color(0x94A162)
+val Voilet = Color(0x7f5af0)
